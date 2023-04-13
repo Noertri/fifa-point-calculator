41de7b4d23bf182c3fbadb780da952ec45ef8e7d
Noertri <trinuruns@gmail.com>
2023-04-13 13:00:33 +0700

    revisi select box

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 const optionsBtn = document.querySelector(".options-button");
-const optionsBtnStyle = getComputedStyle(optionsBtn);
 const optionsList = document.querySelectorAll(".option-item");
 const optionsContainer = document.querySelector(".options-container");
 const selectBox = document.querySelector(".select-box");
@@ -10,20 +9,13 @@ optionsBtn.addEventListener("click", () => {
     optionsContainer.classList.toggle("d-flex");
     optionsContainer.classList.toggle("flex-column");
     optionsContainer.classList.toggle("flex-shrink-1");
-    const currentWidth = optionsBtnStyle.width;
-
-    if (isActivated) {
-        const optionsContainerActive = document.querySelector(".options-container.active");
-        optionsContainerActive.style["max-width"] = currentWidth;
-    }
-
+    
     optionsList.forEach(option => {
         option.addEventListener("click", (e) => {
             let optionsBtnElement = optionsBtn.querySelector("span");
             let optionLabelText = option.querySelector("label").innerText.trim();
             optionsBtnElement.innerText = optionLabelText;
             let optionItem = option.querySelector("input");
-            // console.log(Number(optionItem.value));
             optionsContainer.classList.remove("active");
             optionsContainer.classList.remove("d-flex");
             optionsContainer.classList.remove("flex-column");
@@ -34,3 +26,10 @@ optionsBtn.addEventListener("click", () => {
         })
     })
 });
+
+const matchResults = document.querySelectorAll("input[name='match-result']");
+matchResults.forEach(btn => {
+    btn.addEventListener("click", e => {
+        console.log(btn.value)
+    })
+})
