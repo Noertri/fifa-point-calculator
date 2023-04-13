fa325b756b34a9ed150f4ba73d2220fdeb6766fe
Noertri <trinuruns@gmail.com>
2023-04-13 14:29:34 +0700

    menambah event listener untuk checkbox dan button

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,32 @@ const optionsBtn = document.querySelector(".options-button");
 const optionsList = document.querySelectorAll(".option-item");
 const optionsContainer = document.querySelector(".options-container");
 const selectBox = document.querySelector(".select-box");
+const matchResults = document.querySelectorAll("input[name='match-result']");
+const oldPointA = document.querySelector("#old-point-team-a");
+const oldPointB = document.querySelector("#old-point-team-b");
+const additionalRules1 = document.getElementById("ko-round");
+const additionalRules2 = document.getElementById("penalty-shoot");
+const submitBtn = document.querySelector("#submit-btn");
+const optionItems = document.querySelectorAll("input[name='option-match']");
 
+// inisialisasi
+oldPointA.value = "";
+oldPointB.value = "";
+additionalRules1.checked = false;
+additionalRules2.checked = false;
+
+matchResults.forEach(btn => {
+    btn.checked = false;
+});
+
+optionItems.forEach(option => {
+    option.checked = false;
+});
+
+let data = {};
 
 optionsBtn.addEventListener("click", () => {
-    const isActivated = optionsContainer.classList.toggle("active");
+    optionsContainer.classList.toggle("active");
     optionsContainer.classList.toggle("d-flex");
     optionsContainer.classList.toggle("flex-column");
     optionsContainer.classList.toggle("flex-shrink-1");
@@ -16,20 +38,53 @@ optionsBtn.addEventListener("click", () => {
             let optionLabelText = option.querySelector("label").innerText.trim();
             optionsBtnElement.innerText = optionLabelText;
             let optionItem = option.querySelector("input");
+
+            if (optionItem.checked) {
+                data["matchCoeff"] = Number(optionItem.value);
+                let coeffValue = document.querySelector(".match-coefficient-value");
+                coeffValue.innerText = "Koefisien pertandingan: " + optionItem.value;
+            }
+            
             optionsContainer.classList.remove("active");
             optionsContainer.classList.remove("d-flex");
             optionsContainer.classList.remove("flex-column");
             optionsContainer.classList.remove("flex-shrink-1");
-
-            let coeffValue = document.querySelector(".match-coefficient-value");
-            coeffValue.innerText = "Koefisien pertandingan: " + optionItem.value;
         })
     })
+
 });
 
-const matchResults = document.querySelectorAll("input[name='match-result']");
+
 matchResults.forEach(btn => {
     btn.addEventListener("click", e => {
-        console.log(btn.value)
+        if (btn.checked) {
+            data["matchResult"] = Number(btn.value);
+        }
     })
+})
+
+additionalRules1.addEventListener("click", (e) => {
+    if (additionalRules1.checked) {
+        data["kickOffRound"] = Boolean(additionalRules1.value)
+    }
+})
+
+
+additionalRules2.addEventListener("click", (e) => {
+    if (additionalRules2.checked) {
+        data["pso"] = Boolean(additionalRules2.value)
+    }
+})
+
+
+submitBtn.addEventListener("click", e =>{
+    if (oldPointA.value && oldPointB.value){
+        data["oldPointTeamA"] = Number(oldPointA.value);
+        data["oldPointTeamB"] = Number(oldPointB.value);
+    }
+
+    if (data) {
+        console.log(data);
+    }
+    
 })
