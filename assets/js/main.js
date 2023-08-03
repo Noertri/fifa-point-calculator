512c220a340d3b81c9920a2fdfc64b2a29c205ff
Noertri <trinuruns@gmail.com>
2023-08-03 13:53:24 +0700

    mengganti select menu dengan input text

--- assets/js/main.js
+++ assets/js/main.js
@@ -60,13 +60,13 @@ updateNewPoints(data);
 //     }
 // });
 
-async function loadCountryList(){
-    let response = await fetch("http://127.0.0.1:5000/fifa-point-calculator/api/countryList?periode=2023-06-29");
-    let items = await response.json();
-    console.log(items);
-} 
+// async function loadCountryList(){
+//     let response = await fetch("http://127.0.0.1:5000/fifa-point-calculator/api/countryList?periode=2023-06-29");
+//     let items = await response.json();
+//     console.log(items);
+// } 
 
-loadCountryList();
+// loadCountryList();
 
 optionsBtn.addEventListener("click", () => {
     let toggleClassItems = ["active", "d-flex", "flex-column", "flex-shrink-1"];
@@ -108,141 +108,141 @@ optionsList.forEach(option => {
     })
 })
 
-matchResults.forEach(radioBtn => {
-    radioBtn.addEventListener("click", e => {
-        if (radioBtn.checked) {
-            data["matchResult"] = radioBtn.value;
-        }
-    })
-})
+// matchResults.forEach(radioBtn => {
+//     radioBtn.addEventListener("click", e => {
+//         if (radioBtn.checked) {
+//             data["matchResult"] = radioBtn.value;
+//         }
+//     })
+// })
 
-additionalRules1.addEventListener("click", (e) => {
-    if (additionalRules1.checked) {
-        data["kickOffRound"] = Boolean(additionalRules1.value);
-    } else {
-        data["kickOffRound"] = false;
-    }
-})
+// additionalRules1.addEventListener("click", (e) => {
+//     if (additionalRules1.checked) {
+//         data["kickOffRound"] = Boolean(additionalRules1.value);
+//     } else {
+//         data["kickOffRound"] = false;
+//     }
+// })
 
-additionalRules2.addEventListener("click", (e) => {
-    if (additionalRules2.checked) {
-        data["pso"] = Boolean(additionalRules2.value);
-    } else {
-        data["pso"] = false;
-    }
-})
+// additionalRules2.addEventListener("click", (e) => {
+//     if (additionalRules2.checked) {
+//         data["pso"] = Boolean(additionalRules2.value);
+//     } else {
+//         data["pso"] = false;
+//     }
+// })
 
-submitBtn.addEventListener("click", e =>{
-    if (oldPointA.value && oldPointB.value){
-        data["oldPointTeamA"] = parseFloat(oldPointA.value);
-        data["oldPointTeamB"] = parseFloat(oldPointB.value);
-    }
+// submitBtn.addEventListener("click", e =>{
+//     if (oldPointA.value && oldPointB.value){
+//         data["oldPointTeamA"] = parseFloat(oldPointA.value);
+//         data["oldPointTeamB"] = parseFloat(oldPointB.value);
+//     }
 
-    calcPoints(data);
-})
+//     calcPoints(data);
+// })
 
-function updateMatchExpect(data) {
-    let weA = matchExpectation(data.oldPointTeamA, data.oldPointTeamB);
-    let weB = matchExpectation(data.oldPointTeamB, data.oldPointTeamA);
+// function updateMatchExpect(data) {
+//     let weA = matchExpectation(data.oldPointTeamA, data.oldPointTeamB);
+//     let weB = matchExpectation(data.oldPointTeamB, data.oldPointTeamA);
     
-    data["weA"] = parseFloat(weA.toFixed(5));
-    data["weB"] = parseFloat(weB.toFixed(5));
+//     data["weA"] = parseFloat(weA.toFixed(5));
+//     data["weB"] = parseFloat(weB.toFixed(5));
 
-    let coeffTag = matchExpectWrapper.querySelector("#match-coeff-value");
-    let weATag = matchExpectWrapper.querySelector(".we-a");
-    let weBTag = matchExpectWrapper.querySelector(".we-b");
+//     let coeffTag = matchExpectWrapper.querySelector("#match-coeff-value");
+//     let weATag = matchExpectWrapper.querySelector(".we-a");
+//     let weBTag = matchExpectWrapper.querySelector(".we-b");
     
-    coeffTag.innerText = `Koefisien pertandingan = ${data.matchCoeff}`;
-    weATag.innerText = `We tim A = ${weA.toFixed(5)}`;
-    weBTag.innerText = `We tim B = ${weB.toFixed(5)}`; 
-}
+//     coeffTag.innerText = `Koefisien pertandingan = ${data.matchCoeff}`;
+//     weATag.innerText = `We tim A = ${weA.toFixed(5)}`;
+//     weBTag.innerText = `We tim B = ${weB.toFixed(5)}`; 
+// }
 
-function matchExpectation(point1, point2){
-    let dr = point1 - point2;
-    let we = 1/(10**(-1*(dr/600))+1);
+// function matchExpectation(point1, point2){
+//     let dr = point1 - point2;
+//     let we = 1/(10**(-1*(dr/600))+1);
 
-    return we
-}
+//     return we
+// }
 
-function calcPoints(matchData) {
-    let weA = matchData.weA;
-    let weB = matchData.weB;
+// function calcPoints(matchData) {
+//     let weA = matchData.weA;
+//     let weB = matchData.weB;
 
-    let diffPointTeamA;
-    let diffPointTeamB;
+//     let diffPointTeamA;
+//     let diffPointTeamB;
 
-    switch (matchData.matchResult) {
-        case "win":
-            if (matchData.kickOffRound && matchData.pso) {
-                diffPointTeamA = matchData.matchCoeff*(0.75-weA);
-                diffPointTeamB = matchData.matchCoeff*(0.5-weB);
-            } else if (matchData.kickOffRound) {
-                diffPointTeamA = matchData.matchCoeff*(1-weA);
-                diffPointTeamB = 0.00;
-            } else {
-                diffPointTeamA = matchData.matchCoeff*(1-weA);
-                diffPointTeamB = matchData.matchCoeff*(0-weB);
-            }
-            break
-        case "draw":
-            diffPointTeamA = matchData.matchCoeff*(0.5-weA);
-            diffPointTeamB = matchData.matchCoeff*(0.5-weB);
-            break
-        case "lose":
-            if (matchData.kickOffRound && matchData.pso) {
-                diffPointTeamA = matchData.matchCoeff*(0.5-weA);
-                diffPointTeamB = matchData.matchCoeff*(0.75-weB);
-            } else if (matchData.kickOffRound) {
-                diffPointTeamA = 0.00;
-                diffPointTeamB = matchData.matchCoeff*(1-weB);
-            } else {
-                diffPointTeamA = matchData.matchCoeff*(0-weA);
-                diffPointTeamB = matchData.matchCoeff*(1-weB);
-            }
-            break
-        default:
-            diffPointTeamA = 0.0;
-            diffPointTeamB = 0.0;
-            break
-    }
+//     switch (matchData.matchResult) {
+//         case "win":
+//             if (matchData.kickOffRound && matchData.pso) {
+//                 diffPointTeamA = matchData.matchCoeff*(0.75-weA);
+//                 diffPointTeamB = matchData.matchCoeff*(0.5-weB);
+//             } else if (matchData.kickOffRound) {
+//                 diffPointTeamA = matchData.matchCoeff*(1-weA);
+//                 diffPointTeamB = 0.00;
+//             } else {
+//                 diffPointTeamA = matchData.matchCoeff*(1-weA);
+//                 diffPointTeamB = matchData.matchCoeff*(0-weB);
+//             }
+//             break
+//         case "draw":
+//             diffPointTeamA = matchData.matchCoeff*(0.5-weA);
+//             diffPointTeamB = matchData.matchCoeff*(0.5-weB);
+//             break
+//         case "lose":
+//             if (matchData.kickOffRound && matchData.pso) {
+//                 diffPointTeamA = matchData.matchCoeff*(0.5-weA);
+//                 diffPointTeamB = matchData.matchCoeff*(0.75-weB);
+//             } else if (matchData.kickOffRound) {
+//                 diffPointTeamA = 0.00;
+//                 diffPointTeamB = matchData.matchCoeff*(1-weB);
+//             } else {
+//                 diffPointTeamA = matchData.matchCoeff*(0-weA);
+//                 diffPointTeamB = matchData.matchCoeff*(1-weB);
+//             }
+//             break
+//         default:
+//             diffPointTeamA = 0.0;
+//             diffPointTeamB = 0.0;
+//             break
+//     }
     
-    matchData.newPointTeamA = matchData.oldPointTeamA + parseFloat(diffPointTeamA.toFixed(2));
-    matchData.newPointTeamB = matchData.oldPointTeamB + parseFloat(diffPointTeamB.toFixed(2));
-    matchData.diffPointTeamA = diffPointTeamA;
-    matchData.diffPointTeamB = diffPointTeamB;
+//     matchData.newPointTeamA = matchData.oldPointTeamA + parseFloat(diffPointTeamA.toFixed(2));
+//     matchData.newPointTeamB = matchData.oldPointTeamB + parseFloat(diffPointTeamB.toFixed(2));
+//     matchData.diffPointTeamA = diffPointTeamA;
+//     matchData.diffPointTeamB = diffPointTeamB;
 
-    updateNewPoints(matchData);
-}
+//     updateNewPoints(matchData);
+// }
 
-function updateNewPoints(matchData){
-    let newPointTeamAElem = resultWrapper.querySelector(".new-point.team-a");
-    let newPointTeamBElem = resultWrapper.querySelector(".new-point.team-b");
-    let diffPointTeamAElem = resultWrapper.querySelector(".diff-point.team-a");
-    let diffPointTeamBElem = resultWrapper.querySelector(".diff-point.team-b");
+// function updateNewPoints(matchData){
+//     let newPointTeamAElem = resultWrapper.querySelector(".new-point.team-a");
+//     let newPointTeamBElem = resultWrapper.querySelector(".new-point.team-b");
+//     let diffPointTeamAElem = resultWrapper.querySelector(".diff-point.team-a");
+//     let diffPointTeamBElem = resultWrapper.querySelector(".diff-point.team-b");
 
-    newPointTeamAElem.innerText = `${matchData.newPointTeamA.toFixed(2)}`;
-    newPointTeamBElem.innerText = `${matchData.newPointTeamB.toFixed(2)}`;
+//     newPointTeamAElem.innerText = `${matchData.newPointTeamA.toFixed(2)}`;
+//     newPointTeamBElem.innerText = `${matchData.newPointTeamB.toFixed(2)}`;
 
-    if (matchData.diffPointTeamA >= 0 && matchData.diffPointTeamB < 0) {
-        diffPointTeamAElem.innerText = `+${matchData.diffPointTeamA.toFixed(2)}`;
-        diffPointTeamBElem.innerText = `${matchData.diffPointTeamB.toFixed(2)}`;
-        newPointTeamAElem.style.color = "rgb(7, 177, 7)";
-        diffPointTeamAElem.style.color = "rgb(7, 177, 7)";
-        newPointTeamBElem.style.color = "red";
-        diffPointTeamBElem.style.color = "red";
-    } else if (matchData.diffPointTeamA < 0 && matchData.diffPointTeamB >= 0){
-        diffPointTeamAElem.innerText = `${matchData.diffPointTeamA.toFixed(2)}`;
-        diffPointTeamBElem.innerText = `+${matchData.diffPointTeamB.toFixed(2)}`;
-        newPointTeamAElem.style.color = "red";
-        diffPointTeamAElem.style.color = "red";
-        newPointTeamBElem.style.color = "rgb(7, 177, 7)";
-        diffPointTeamBElem.style.color = "rgb(7, 177, 7)";
-    } else if (matchData.diffPointTeamA >= 0 && matchData.diffPointTeamB >= 0) {
-        diffPointTeamAElem.innerText = `+${matchData.diffPointTeamA.toFixed(2)}`;
-        diffPointTeamBElem.innerText = `+${matchData.diffPointTeamB.toFixed(2)}`;
-        newPointTeamAElem.style.color = "rgb(7, 177, 7)";
-        diffPointTeamAElem.style.color = "rgb(7, 177, 7)";
-        newPointTeamBElem.style.color = "rgb(7, 177, 7)";
-        diffPointTeamBElem.style.color = "rgb(7, 177, 7)";
-    }
-}
+//     if (matchData.diffPointTeamA >= 0 && matchData.diffPointTeamB < 0) {
+//         diffPointTeamAElem.innerText = `+${matchData.diffPointTeamA.toFixed(2)}`;
+//         diffPointTeamBElem.innerText = `${matchData.diffPointTeamB.toFixed(2)}`;
+//         newPointTeamAElem.style.color = "rgb(7, 177, 7)";
+//         diffPointTeamAElem.style.color = "rgb(7, 177, 7)";
+//         newPointTeamBElem.style.color = "red";
+//         diffPointTeamBElem.style.color = "red";
+//     } else if (matchData.diffPointTeamA < 0 && matchData.diffPointTeamB >= 0){
+//         diffPointTeamAElem.innerText = `${matchData.diffPointTeamA.toFixed(2)}`;
+//         diffPointTeamBElem.innerText = `+${matchData.diffPointTeamB.toFixed(2)}`;
+//         newPointTeamAElem.style.color = "red";
+//         diffPointTeamAElem.style.color = "red";
+//         newPointTeamBElem.style.color = "rgb(7, 177, 7)";
+//         diffPointTeamBElem.style.color = "rgb(7, 177, 7)";
+//     } else if (matchData.diffPointTeamA >= 0 && matchData.diffPointTeamB >= 0) {
+//         diffPointTeamAElem.innerText = `+${matchData.diffPointTeamA.toFixed(2)}`;
+//         diffPointTeamBElem.innerText = `+${matchData.diffPointTeamB.toFixed(2)}`;
+//         newPointTeamAElem.style.color = "rgb(7, 177, 7)";
+//         diffPointTeamAElem.style.color = "rgb(7, 177, 7)";
+//         newPointTeamBElem.style.color = "rgb(7, 177, 7)";
+//         diffPointTeamBElem.style.color = "rgb(7, 177, 7)";
+//     }
+// }
